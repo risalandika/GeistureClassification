0f574a5f329442387c62fe29c3c565343e11a4b8
Risal Andika Tridisaputra <risal.andika@gmail.com>
2014-12-01 11:52:22 +0700

    Membuat Visual Studio 2012 Solution

---
+++ GeistClass/GeistClass/LeapEventListener.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap;
+
+namespace GeistClass
+{
+    class LeapEventListener : Listener
+    {
+        ILeapEventDelegate eventDelegate;
+
+        public LeapEventListener(ILeapEventDelegate delegateObject)
+        {
+            this.eventDelegate = delegateObject;
+        }
+        public override void OnInit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onInit");
+        }
+        public override void OnConnect(Controller controller)
+        {
+            controller.SetPolicy(Controller.PolicyFlag.POLICY_IMAGES);
+            controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+            this.eventDelegate.LeapEventNotification("onConnect");
+        }
+
+        public override void OnFrame(Controller controller)
+        {
+            //Toleransi Gerakan
+            if (controller.Frame().Fingers[0].TipVelocity.Magnitude <= 25) return;
+            else
+            {
+                this.eventDelegate.LeapEventNotification("onFrame");
+            }
+        }
+        public override void OnExit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onExit");
+        }
+        public override void OnDisconnect(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onDisconnect");
+        }
+    }
+}
---
+++ GeistClass/GeistClass/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Leap;
+
+namespace GeistClass
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ILeapEventDelegate
+    {
+        private Controller controller = new Controller();
+        private LeapEventListener listener;
+        delegate void LeapEventDelegate(string EventName);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.controller = new Controller();
+            this.listener = new LeapEventListener(this);
+            controller.AddListener(listener);
+        }
+
+        public void LeapEventNotification(string EventName)
+        {
+            if (this.CheckAccess())
+            {
+                switch (EventName)
+                {
+                    case "onInit":
+                        Debug.WriteLine("Init");
+                        break;
+                    case "onConnect":
+                        this.connectHandler();
+                        break;
+                    case "onFrame":
+                        this.newFrameHandler(this.controller.Frame());
+                        break;
+                }
+            }
+            else
+            {
+                Dispatcher.Invoke(new LeapEventDelegate(LeapEventNotification), new object[] { EventName });
+            }
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            this.newFrameHandler(this.controller.Frame());
+            Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+        }
+
+        void connectHandler()
+        {
+            this.controller.SetPolicy(Controller.PolicyFlag.POLICY_IMAGES);
+            this.controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+            this.controller.Config.SetFloat("Gesture.Swipe.MinLength", 100.0f);
+        }
+
+        //Handle setiap frame
+        void newFrameHandler(Leap.Frame frame)
+        {
+            Console.WriteLine("ID : " + frame.Id.ToString());
+            Console.WriteLine("Finger Count : " + frame.Fingers.Count.ToString());
+        }
+    }
+}
